e33a0688c60da132a31ef1884136cbf56ea729a5
MarianoGuastella <marianoguastella@hotmail.com>
2023-12-23 19:51:36 -0300

    style: refine visual styling and user experience
    In this commit, visual improvements and styling changes have been applied to the HTML, CSS, and JS files:
    
    - Set a background image that covers the entire window
    - Adjusted fonts, margins, and padding for improved readability
    - Enhanced button styles, including hover effects
    - Improved responsiveness for various screen sizes
    
    These changes contribute to a more professional and visually appealing NLP Emotion Detection website.

--- server.py
+++ server.py
@@ -7,6 +7,18 @@ from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
+def map_sentiment_label(sentiment_label):
+    label_mapping = {
+        'P+': 'Strong Positive',
+        'P': 'Positive',
+        'NEU': 'Neutral',
+        'N': 'Negative',
+        'N+': 'Strong Negative',
+        'NONE': 'Without Polarity'
+    }
+
+    return label_mapping.get(sentiment_label, sentiment_label)
+
 @app.route("/emotionDetector")
 def emo_detector():
     ''' This code receives the text from the HTML interface and 
@@ -14,25 +26,27 @@ def emo_detector():
         function. The output returned shows the emotions, their score 
         and the dominant emotion from the text.
     '''
-    scoring_table = """[P+: strong positive, 
-    P: positive, 
-    NEU: neutral, 
-    N: negative, 
-    N+: strong negative, 
-    NONE: without polarity]"""
     text_to_analyze = request.args.get("textToAnalyze")
     sentiment_info = emotion_detector(text_to_analyze)
     status = sentiment_info['status']['msg']
+    
     if status != 'OK':
         return f"Invalid text! Please try again!. Error {status}"
+
     sentiment_score = sentiment_info.get('score_tag', '')
     sentimented_entity_list = sentiment_info.get('sentimented_entity_list', [])
+
     if sentimented_entity_list:
         sentiment_label = sentimented_entity_list[0].get('form', '')
+        sentiment_label = map_sentiment_label(sentiment_label)
     else:
         sentiment_label = "No sentimented entities found."
-    formatted_response = f"For the given statement and knowing the range of responses {scoring_table}, the system response is Score : {sentiment_score} and label : {sentiment_label}"
-    print(formatted_response)
+
+    formatted_response = (
+    "For the given statement, the system response is:\n"
+    f"Score: {sentiment_score}\n"
+    f"Label: {sentiment_label}"
+    )
     return formatted_response
 
 @app.route("/")
---
+++ static/js/mywebscript.js
@@ -0,0 +1,20 @@
+function displayResponse(response) {
+    // Replace \n with <br> for line breaks
+    response = response.replace(/\n/g, '<br>');
+    document.getElementById("system_response").innerHTML = response;
+}
+
+let RunSentimentAnalysis = () => {
+    showLoading();
+    textToAnalyze = document.getElementById("textToAnalyze").value;
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            displayResponse(xhttp.responseText);
+            hideLoading();
+        }
+    };
+    xhttp.open("GET", "emotionDetector?textToAnalyze=" + encodeURIComponent(textToAnalyze), true);
+    xhttp.send();
+}
